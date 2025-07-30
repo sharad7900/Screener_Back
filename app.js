490fbee7fe35771085d5e21916642ae05af14234
mongodb <mongodbid67@gmail.com>
2025-07-30 16:07:37 +0530

    Cors

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = require('./Router/route.js');
+const cors = require('cors');
+
 
 
 
 const app = express();
 app.use(express.json());
-
-
+const corsOption ={
+    origin:"https://screener-rust.vercel.app/",
+    methods: "GET, PUT, PATCH, DELETE, POST, HEAD",
+    credentials:true
+}
+app.use(cors(corsOption));
 app.use("/",router);
+
+
 
 
 app.listen(5000,()=>{
