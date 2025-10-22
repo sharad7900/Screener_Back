80b4246e53c975a4a322748ad6315fb4e6da2038
mongodb <mongodbid67@gmail.com>
2025-10-22 10:55:03 +0530

    ENV Adding

--- Utils/SQL.js
+++ Utils/SQL.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'mysql-sharad.alwaysdata.net',
-  user: 'sharad',
-  password: '10101212Rs',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
   waitForConnections: true,
   connectionLimit: 10
 });
