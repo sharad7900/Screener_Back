10d8c76568bba6a20cc5562a5e24fd96b075e2ba
mongodb <mongodbid67@gmail.com>
2025-10-22 11:07:43 +0530

    Route fix

--- Router/route.js
+++ Router/route.js
@@ -6,7 +6,6 @@ const {MFPage} = require("../Controllers/MFPage.js");
 
 router.route("/").get(controller.table_data);
 router.route("/MFinfo").post(MFPage);
-router.route("/heatmap").get(require("../Controllers/Heatmap.js").getHeatmapData);
 
 
 module.exports = router;
